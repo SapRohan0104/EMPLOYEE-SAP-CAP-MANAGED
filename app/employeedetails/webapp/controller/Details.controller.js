fe705cbc45b3b2c8294f28ea24eb43ab9a544743
Rohan Sharma <rohan.sharmasap0104@gmail.com>
2024-04-24 13:42:27 +0000

    Router added with element binding

--- app/employeedetails/webapp/controller/Details.controller.js
+++ app/employeedetails/webapp/controller/Details.controller.js
@@ -7,6 +7,18 @@ sap.ui.define(
   
       return BaseController.extend("com.app.employeedetails.controller.Details", {
         onInit: function() {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.attachRoutePatternMatched(this.onEmployeeDetailsLoad, this); 
+        },
+        onEmployeeDetailsLoad: function(oEvent ){
+            const {empId} = oEvent.getParameter("arguments");
+            const sRouterName = oEvent.getParameter("name");
+            const oForm = this.getView().byId("idEmployeeDetailsForm");
+
+            oForm.bindElement(`/Employee(${empId})`, {
+                expand: 'salary,address'
+            });
+            debugger;
         }
       });
     }
